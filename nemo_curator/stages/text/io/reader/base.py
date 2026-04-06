e89b14e1212791841951ec8c23a1bb580d93668c
Sarah Yurick <53962159+sarahyurick@users.noreply.github.com>
2026-04-06 14:25:32 -0700

    Improve error messages on large file reads (#1630)
    * Improve error messages on large file reads
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * improved logic
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * be more explicit about data type
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * ruff
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * use file partitioning stage check, make configurable params
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * remove outdated tests
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * greptile
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * more greptile comments
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * more greptile complaints
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * add a test
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * add jsonl and parquet tests
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * use blocksize as a catchall
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * update tests
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * fix test
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * update comments
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * improve docstrings and messaging
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * other small complaints
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * change from error to warning
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * fix tests
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * remove in-memory check
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * fix ruff
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * remove blocksize from basereader
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * update recommended default from 2gb to 512mb
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * change error to warning
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    ---------
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>

--- nemo_curator/stages/text/io/reader/base.py
+++ nemo_curator/stages/text/io/reader/base.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -77,11 +77,13 @@ class BaseReader(ProcessingStage[FileGroupTask, DocumentBatch]):
         effective_read_kwargs: dict[str, Any] = {}
         if self.read_kwargs:
             effective_read_kwargs.update(self.read_kwargs)
+
         # Read the files
         result = self.read_data(task.data, effective_read_kwargs, self.fields)
-        # Validate
+
+        # Validate the result
         if (
-            result is None
+            (result is None)
             or (hasattr(result, "empty") and result.empty)
             or (hasattr(result, "num_rows") and result.num_rows == 0)
         ):
